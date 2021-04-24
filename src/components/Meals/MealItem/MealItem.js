837bcc96293435a66441590a95fb4abaa324052e
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-04-24 13:01:29 +0200

    I Added Forward Refs to use custom Input components. (close #8)

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
+import { useContext } from "react";
 import MealItemForm from "./MealItemForm";
-import classes from './MealItem.module.css';
+import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
- const price = `$${props.price.toFixed(2)}`;
- 
-    return (
+  const cartCtx = useContext(CartContext);
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id:props.id,
+      name:props.name,
+      amount:amount, 
+      price:props.price
+    })
+  };
+  return (
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
@@ -12,12 +23,11 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm/>
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
 };
 export default MealItem;
 
-
-console.log(`Hello`, "Hello", 'Hello ');
+console.log(`Hello`, "Hello", "Hello ");
