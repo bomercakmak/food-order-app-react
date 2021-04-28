f919383ed73e1dc797d20ff6a49931cd435ac387
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-04-28 13:34:53 +0200

    I added Items Removable functions.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,8 +9,12 @@ const Cart = (props) => {
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const carItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const carItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id)
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1})
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -20,8 +24,8 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={carItemRemoveHandler}
-          onAdd={cartItemAddHandler}
+          onRemove={() => carItemRemoveHandler(item.id)}
+          onAdd={() => cartItemAddHandler(item)}
         />
       ))}
     </ul>
